448b41151916592a5647384cf5e32dd557f6f1e7
MANBEERGIL <manbeergill545@gmail.com>
2020-03-05 21:07:13 -0500

    Initial Commit

---
+++ C0768706_MIDTerm_MAD3115W2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0768706_MIDTerm_MAD3115W2020
+//
+//  Created by MANBEER KAUR on 2020-03-05.
+//  Copyright © 2020 MANBEER KAUR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
