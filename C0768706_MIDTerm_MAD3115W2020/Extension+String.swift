4359328f6ccce6f092814cd12764b5fdcfe169c5
MANBEERGIL <manbeergill545@gmail.com>
2020-03-07 10:22:07 -0500

    customer view controller

---
+++ C0768706_MIDTerm_MAD3115W2020/Extension+String.swift
@@ -0,0 +1,22 @@
+//
+//  Extension+String.swift
+//  C0768706_MIDTerm_MAD3115W2020
+//
+//  Created by MANBEER KAUR on 2020-03-07.
+//  Copyright © 2020 MANBEER KAUR. All rights reserved.
+//
+
+import Foundation
+extension String{
+    func emailValid() -> Bool{
+        let emailRegEx = "[A-Z0-9a-z.%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+    
+    func mobileValid() -> Bool{         
+        let mobileNumberRegEx = "[0-9]{10}"
+        let mobileTest = NSPredicate(format: "SELF MATCHES %@", mobileNumberRegEx)
+        return mobileTest.evaluate(with: self)
+    }
+}
